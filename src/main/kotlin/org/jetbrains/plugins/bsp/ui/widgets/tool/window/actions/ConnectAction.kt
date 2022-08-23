3e25b668c6dcbe2ebf9ce4a553d33550bc00f56f
azdrojowa123 <aleksandra.zdrojowa@jetbrains.com>
2022-08-23 15:02:06 +0200

    upgrade connection action

--- src/main/kotlin/org/jetbrains/plugins/bsp/ui/widgets/tool/window/actions/ConnectAction.kt
+++ src/main/kotlin/org/jetbrains/plugins/bsp/ui/widgets/tool/window/actions/ConnectAction.kt
@@ -2,11 +2,13 @@ package org.jetbrains.plugins.bsp.ui.widgets.tool.window.actions
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.progress.runBackgroundableTask
 import com.intellij.project.stateStore
 import org.jetbrains.plugins.bsp.services.BspBuildConsoleService
 import org.jetbrains.plugins.bsp.services.BspConnectionService
 import org.jetbrains.plugins.bsp.services.BspSyncConsoleService
+import org.jetbrains.plugins.bsp.services.MagicMetaModelService
 import org.jetbrains.plugins.bsp.services.VeryTemporaryBspResolver
 import org.jetbrains.plugins.bsp.ui.widgets.tool.window.all.targets.BspAllTargetsWidgetBundle
 
@@ -17,14 +19,21 @@ public class ConnectAction : AnAction(BspAllTargetsWidgetBundle.message("connect
     val bspConnectionService = project.getService(BspConnectionService::class.java)
     val bspSyncConsoleService = BspSyncConsoleService.getInstance(project)
     val bspBuildConsoleService = BspBuildConsoleService.getInstance(project)
+    val magicMetaModelService = MagicMetaModelService.getInstance(project)
 
     runBackgroundableTask("Connect action", project) {
       bspConnectionService.reconnect(project.locationHash)
       val bspResolver =
         VeryTemporaryBspResolver(project.stateStore.projectBasePath, bspConnectionService.server!!, bspSyncConsoleService.bspSyncConsole, bspBuildConsoleService.bspBuildConsole)
-      bspResolver.collectModel()
-    }
+      val projectDetails = bspResolver.collectModel()
 
+      magicMetaModelService.initializeMagicModel(projectDetails)
+      val magicMetaModel = magicMetaModelService.magicMetaModel
+      magicMetaModel.loadDefaultTargets()
+      runWriteAction {
+        magicMetaModel.save()
+      }
+    }
   }
 
   public override fun update(e: AnActionEvent) {
