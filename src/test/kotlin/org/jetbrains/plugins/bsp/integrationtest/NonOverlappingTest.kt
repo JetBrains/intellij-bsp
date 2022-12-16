6bfdebeef19f295457c8b8f4737af3af42cb9ffc
Tomasz Pasternak <tomasz.pasternak@jetbrains.com>
2022-12-16 11:59:13 +0100

    Make sure BSP server is killed
    Apart from this, bump Bazel repo to 5.4.0 and
    increase project view size

--- src/test/kotlin/org/jetbrains/plugins/bsp/integrationtest/NonOverlappingTest.kt
+++ src/test/kotlin/org/jetbrains/plugins/bsp/integrationtest/NonOverlappingTest.kt
@@ -18,26 +18,31 @@ import kotlin.io.path.createTempDirectory
 import kotlin.io.path.readText
 
 // TODO make sure these values are updated by dependabot or similar tool
-private const val bazelRepositoryTag = "5.3.2"
+private const val bazelRepositoryTag = "5.4.0"
 private const val bazelBspVersion = "2.3.0"
 class NonOverlappingTest {
   @Test
   fun `Compute non overlapping targets for bazelbuild_bazel project`() {
     val bazelDir = createTempDirectory("bazel-bsp-")
     cloneRepository(bazelDir)
-    installBsp(bazelDir, "//src/tools/...")
+    installBsp(bazelDir, "//src/test/java/...")
     val connectionDetails = Gson().fromJson(bazelDir.resolve(".bsp/bazelbsp.json").readText(), BspConnectionDetails::class.java)
-    val launcher = startBsp(connectionDetails, bazelDir)
-    val params = initializeParams(bazelDir)
-    launcher.startListening()
-    val server = launcher.remoteProxy
-    val initializationResult = server.buildInitialize(params).get()
-    server.onBuildInitialized()
-    val projectDetails = calculateProjectDetailsWithCapabilities(server, initializationResult.capabilities){}
-    val targetsDetailsForDocumentProvider = TargetsDetailsForDocumentProvider(projectDetails.sources)
-    val overlappingTargetsGraph = OverlappingTargetsGraph(targetsDetailsForDocumentProvider)
-    val nonOverlapping = NonOverlappingTargets(projectDetails.targets, overlappingTargetsGraph)
-    nonOverlapping.size shouldBe 60
+    val bspServerProcess = bspProcess(connectionDetails, bazelDir)
+    try {
+      val launcher = startBsp(bspServerProcess)
+      val params = initializeParams(bazelDir)
+      launcher.startListening()
+      val server = launcher.remoteProxy
+      val initializationResult = server.buildInitialize(params).get()
+      server.onBuildInitialized()
+      val projectDetails = calculateProjectDetailsWithCapabilities(server, initializationResult.capabilities) { println(it) }
+      val targetsDetailsForDocumentProvider = TargetsDetailsForDocumentProvider(projectDetails.sources)
+      val overlappingTargetsGraph = OverlappingTargetsGraph(targetsDetailsForDocumentProvider)
+      val nonOverlapping = NonOverlappingTargets(projectDetails.targets, overlappingTargetsGraph)
+      nonOverlapping.size shouldBe 258
+    } finally {
+        bspServerProcess.destroyForcibly()
+    }
   }
 
   private fun cloneRepository(bazelDir: Path) {
@@ -76,11 +81,7 @@ class NonOverlappingTest {
     BuildClientCapabilities(listOf("java"))
   )
 
-  private fun startBsp(connectionDetails: BspConnectionDetails, bazelDir: Path): Launcher<BspServer> {
-    val bspServerProcess = ProcessBuilder(connectionDetails.argv)
-      .directory(bazelDir.toFile())
-      .withRealEnvs()
-      .start()
+  private fun startBsp(bspServerProcess: Process): Launcher<BspServer> {
     return Launcher.Builder<BspServer>()
       .setRemoteInterface(BspServer::class.java)
       .setExecutorService(Executors.newFixedThreadPool(4))
@@ -88,6 +89,13 @@ class NonOverlappingTest {
       .setOutput(bspServerProcess.outputStream)
       .setLocalService(DummyClient())
       .create()
+  }
+
+  private fun bspProcess(connectionDetails: BspConnectionDetails, bazelDir: Path): Process {
+    return ProcessBuilder(connectionDetails.argv)
+      .directory(bazelDir.toFile())
+      .withRealEnvs()
+      .start()
   }
 }
 
