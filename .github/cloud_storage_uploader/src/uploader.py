186f9208e4852dbfe49f4b2861f46d6e80b80cdb
Przemysław Fuchs <fuchs.przemyslaw@gmail.com>
2024-03-07 20:17:15 +0100

    Read ZIP file as bytes

--- .github/cloud_storage_uploader/src/uploader.py
+++ .github/cloud_storage_uploader/src/uploader.py
@@ -8,7 +8,7 @@ class StorageUploader:
         self._storage_client = Client(project=project_name)
 
     def upload_file_to_bucket(self, bucket_name: str, blob_name: str, file_path: str):
-        with open(file_path) as f:
+        with open(file_path, "rb") as f:
             bucket: Bucket = self._storage_client.bucket(bucket_name)
             blob: Blob = bucket.blob(blob_name)
-            blob.upload_from_file(f, content_type='application/zip')
+            blob.upload_from_file(f)
